0568e51d8cf34b23e88f64ac8b62830d366116a5
AliNijat <67079299+AliNijat@users.noreply.github.com>
2020-11-01 15:31:41 -0500

    Branch added

--- src/test/java/SuperBoy.java
+++ src/test/java/SuperBoy.java
@@ -3,6 +3,7 @@ public class SuperBoy {
 
     public static void main(String[] args) {
         System.out.println("If your are a SuperGirl, so I am a Superboy!");
+        //My Group name is Ali_G22
 
     }
 }
